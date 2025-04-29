5655b0ca3bb25e5f55316445409a3a12d3807c7f
nixon42 <aliepcahyo@gmail.com>
2025-04-29 17:16:06 +0700

    add setup to run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+PWD=$(pwd)
+
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv venv
+    sudo ln -s $PWD/wa-bot_server-mon.service /etc/systemd/system/wa-bot_server-mon.service
+    sudo systemctl daemon-reload
+    sudo systemctl enable wa-bot_server-mon.service
+    sudo systemctl start wa-bot_server-mon.service
+    echo "Service installed and started."
+fi
 
 . venv/bin/activate
 . set_env.prop.sh
